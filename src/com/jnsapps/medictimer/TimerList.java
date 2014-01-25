cb4956f41b6d59e75c7b374ffd90531f2d4e7fce
Joaquín Navarro <joaquin.navarro.s@gmail.com>
2014-01-25 11:27:04 +0100

    first commit

---
+++ src/com/jnsapps/medictimer/TimerList.java
@@ -0,0 +1,22 @@
+package com.jnsapps.medictimer;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TimerList extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timer_list);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.timer_list, menu);
+		return true;
+	}
+
+}
